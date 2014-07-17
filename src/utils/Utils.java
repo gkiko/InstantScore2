461a4b37601122ee45da4d9852d272db1faf67e4
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2014-07-17 15:30:05 +0400

    overrode hashCode()

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -2,9 +2,11 @@ package utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Random;
 
 public class Utils {
 	private static String pattern = "yyyy-MM-dd";
+	private static Random random = new Random();
 	
 	public static Calendar getDateWithOffset(int offset){
 		Calendar cal = Calendar.getInstance();
@@ -15,5 +17,14 @@ public class Utils {
 	public static String dateToString(Calendar cal){
 		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
 		return sdf.format(cal.getTime());
+	}
+	
+	public static String generateCode(int length){
+	    char[] digits = new char[length];
+	    digits[0] = (char) (random.nextInt(9) + '1');
+	    for (int i = 1; i < length; i++) {
+	        digits[i] = (char) (random.nextInt(10) + '0');
+	    }
+	    return new String(digits);
 	}
 }
